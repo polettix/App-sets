de58ab929e4f991e3371caa40878c1b38042ff68
Flavio Poletti <flavio@polettix.it>
2012-04-01 21:02:04 +0200

    put submodules into own files

--- bin/pack.sh
+++ bin/pack.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-mobundle -I lib -PB bin/sets -m App::Sets -m Log::Log4perl::Tiny > sets
+mobundle -I lib -PB bin/sets \
+   -m App::Sets \
+   -m App::Sets::Parser \
+   -m App::Sets::Iterator \
+   -m Log::Log4perl::Tiny \
+   > sets
 chmod +x sets
---
+++ lib/App/Sets/Iterator.pm
@@ -0,0 +1,28 @@
+package App::Sets::Iterator;
+use strict;
+use warnings;
+
+sub new {
+   my ($package, $it) = @_;
+   return bless {it => $it}, $package;
+}
+
+sub head {
+   my ($self) = @_;
+   return exists $self->{head} ? $self->{head} : $self->next();
+}
+
+sub next {
+   my ($self) = @_;
+   return $self->{head} = $self->{it}->();
+}
+
+sub drop {
+   my ($self) = @_;
+   my $retval = $self->head();
+   $self->next();
+   return $retval;
+} ## end sub drop
+
+1;
+__END__
