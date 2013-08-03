ee8c6fa44b91260d78fbedc3652a5fa2a12bab02
Flavio Poletti <flavio@polettix.it>
2013-08-03 16:24:25 +0200

    added re-generation from last CPAN distribution

--- bin/pack.sh
+++ bin/pack.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-mobundle -I lib -PB bin/sets \
-   -m App::Sets \
-   -m App::Sets::Parser \
-   -m App::Sets::Iterator \
-   -m Log::Log4perl::Tiny \
-   > sets
+version=$(git tag | tail -1 | sed 's/^v//')
+
+V=$version dzil build
+
+(
+   cd "App-Sets-$version"
+   mobundle -I lib -PB bin/sets \
+      -m App::Sets \
+      -m App::Sets::Parser \
+      -m App::Sets::Iterator \
+      -m Log::Log4perl::Tiny
+) > sets
 chmod +x sets
+
+dzil clean
